210c42ef340b8db2d6d4e3fe9165c8e9626f1fec
Stefan Tagarski <116569281+stefantagarski@users.noreply.github.com>
2023-12-08 20:23:26 +0100

    Add files via upload

---
+++ lab1/zadaca2/RomanConverterTest.java
@@ -0,0 +1,93 @@
+package zadaca2;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class RomanConverterTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        IntStream.range(0, n)
+                .forEach(x -> System.out.println(RomanConverter.toRoman(scanner.nextInt())));
+        scanner.close();
+    }
+}
+
+
+class RomanConverter {
+    /**
+     * Roman to decimal converter
+     *
+     * @param n number in decimal format
+     * @return string representation of the number in Roman numeral
+     */
+    public static String toRoman(int n) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sb.insert(0, convertDigit(digit * (int) Math.pow(10, i)));
+            n /= 10;
+            i++;
+        }
+        return sb.toString();
+    }
+
+    public static String convertDigit(int input) {
+        StringBuilder s = new StringBuilder();
+        while (input >= 1000) {
+            s.append("M");
+            input -= 1000;        }
+        while (input >= 900) {
+            s.append("CM");
+            input -= 900;
+        }
+        while (input >= 500) {
+            s.append("D");
+            input -= 500;
+        }
+        while (input >= 400) {
+            s.append("CD");
+            input -= 400;
+        }
+        while (input >= 100) {
+            s.append("C");
+            input -= 100;
+        }
+        while (input >= 90) {
+            s.append("XC");
+            input -= 90;
+        }
+        while (input >= 50) {
+            s.append("L");
+            input -= 50;
+        }
+        while (input >= 40) {
+            s.append("XL");
+            input -= 40;
+        }
+        while (input >= 10) {
+            s.append("X");
+            input -= 10;
+        }
+        while (input >= 9) {
+            s.append("IX");
+            input -= 9;
+        }
+        while (input >= 5) {
+            s.append("V");
+            input -= 5;
+        }
+        while (input >= 4) {
+            s.append("IV");
+            input -= 4;
+        }
+        while (input >= 1) {
+            s.append("I");
+            input -= 1;
+        }
+        return s.toString();
+    }
+}
+
+
